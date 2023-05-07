600fa5b160fbe6b0c696bf4694b2cf31cc3356d0
antonioggt <antonioggt85@gmail.com>
2023-05-07 08:40:44 -0300

    [

--- src/utils/talkerFsFunction.js
+++ src/utils/talkerFsFunction.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
+
 const router = express.Router();
 
 const talkerPath = path.resolve(__dirname, './talker.json');
@@ -8,13 +9,12 @@ const talkerPath = path.resolve(__dirname, './talker.json');
 router.get('/', async (_req, res) => {
     try {
       const resp = await fs.readFile(talkerPath, 'utf-8');
-      const parsedResp = JSON.parse(resp)
-      return res.status(200).json(parsedResp)
+      const parsedResp = JSON.parse(resp);
+      return res.status(200).json(parsedResp);
     } catch (error) {
       console.error(error);
       return res.status(200).json([]);
     }
-})
+});
 
 module.exports = router;
-
