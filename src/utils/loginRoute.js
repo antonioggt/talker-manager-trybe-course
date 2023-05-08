7cf218d082f1b55f3362b462e570c9623605f9d1
antonioggt <antonioggt85@gmail.com>
2023-05-08 10:54:11 -0300

    [fix] loginRoute.js

--- src/utils/loginRoute.js
+++ src/utils/loginRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const randomToken = require('./tokenGenerator');
+const crypto = require('crypto');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
+  const randomToken = crypto.randomBytes(8).toString('hex');
   if (email && password) {
     res.status(200).json({ token: `${randomToken}` });
   }
